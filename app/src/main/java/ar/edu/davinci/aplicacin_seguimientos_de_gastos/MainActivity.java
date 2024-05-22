7c78901bf5211c9f9ba00750fe28e4180ee268ee
pablo-cendali-davinci <89209028+pablo-cendali-davinci@users.noreply.github.com>
2024-05-22 16:53:20 -0300

    Agrego ingresar cantidad,spinner-categorias

--- app/src/main/java/ar/edu/davinci/aplicacin_seguimientos_de_gastos/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/aplicacin_seguimientos_de_gastos/MainActivity.java
@@ -1,6 +1,13 @@
 package ar.edu.davinci.aplicacin_seguimientos_de_gastos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +15,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spinnerCategorias;
+    private ImageButton btnRegreso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +30,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+        spinnerCategorias= findViewById(R.id.spinner_categorias);
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.categorias, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerCategorias.setAdapter(adapter);
+
+        spinnerCategorias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String seleccionarCategoria = parent.getItemAtPosition(position).toString();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //accion al no seleccionar nada
+            }
+        });
+
+        btnRegreso = findViewById(R.id.btn_regreso);
+        btnRegreso.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Volviendo al login", Toast.LENGTH_SHORT).show();
+        });
+
+
     }
 }
