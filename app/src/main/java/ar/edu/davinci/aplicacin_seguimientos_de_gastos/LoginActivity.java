27ab6866c6c1829ff7614e8b893516fac7160cc6
pablo-cendali-davinci <89209028+pablo-cendali-davinci@users.noreply.github.com>
2024-07-03 03:08:42 -0300

    Defino la activity_login.xml,y LoginActivity.java

--- app/src/main/java/ar/edu/davinci/aplicacin_seguimientos_de_gastos/LoginActivity.java
+++ app/src/main/java/ar/edu/davinci/aplicacin_seguimientos_de_gastos/LoginActivity.java
@@ -2,6 +2,7 @@ package ar.edu.davinci.aplicacin_seguimientos_de_gastos;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,12 +32,14 @@ public class LoginActivity extends AppCompatActivity {
 
                 // Validación simple de usuario y contraseña
                 if (usuario.equals("pablo") && contrasena.equals("pablo")) {
-                    // Autenticación exitosa, navega al menú principal que todavia no esta programado
+                    // Autenticación exitosa, navega al menú principal
+                    Log.d("LoginActivity", "Autenticación exitosa");
                     Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
                     startActivity(intent);
                     finish();
                 } else {
                     // Autenticación fallida
+                    Log.d("LoginActivity", "Autenticación fallida");
                     Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
                 }
             }
