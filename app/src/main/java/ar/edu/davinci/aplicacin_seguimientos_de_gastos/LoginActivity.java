3124b847c3bedddf2e8abaca424cd6d9c836c5a5
pablo-cendali-davinci <89209028+pablo-cendali-davinci@users.noreply.github.com>
2024-08-06 19:30:59 -0300

    Agrego integración con firebase auth para la autenticación del login y register

--- app/src/main/java/ar/edu/davinci/aplicacin_seguimientos_de_gastos/LoginActivity.java
+++ app/src/main/java/ar/edu/davinci/aplicacin_seguimientos_de_gastos/LoginActivity.java
@@ -6,43 +6,93 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText etUsuario;
+    private EditText etEmail;
     private EditText etContrasena;
     private Button btnLogin;
+    private Button btnGoRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etUsuario = findViewById(R.id.etUsuario);
+        mAuth = FirebaseAuth.getInstance();
+
+        etEmail = findViewById(R.id.etCorreo);
         etContrasena = findViewById(R.id.etContrasena);
         btnLogin = findViewById(R.id.btnLogin);
+        TextView btnGoRegister = findViewById(R.id.btn_GoRegister);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String usuario = etUsuario.getText().toString();
-                String contrasena = etContrasena.getText().toString();
+                String emaiUser = etEmail.getText().toString().trim();
+                String passUser = etContrasena.getText().toString().trim();
 
-                // Validación simple de usuario y contraseña
-                if (usuario.equals("pablo") && contrasena.equals("pablo")) {
-                    // Autenticación exitosa, navega al menú principal
-                    Log.d("LoginActivity", "Autenticación exitosa");
-                    Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    // Autenticación fallida
-                    Log.d("LoginActivity", "Autenticación fallida");
-                    Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                if (emaiUser.isEmpty() && passUser.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Completar los datos solicitados", Toast.LENGTH_SHORT).show();
+                }else{
+                    loginUser(emaiUser, passUser);
                 }
             }
         });
+
+        btnGoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void loginUser(String emailUser, String passUser){
+
+        mAuth.signInWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    finish();
+                    startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+                    Toast.makeText(LoginActivity.this, "Logueado correctamente", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null){
+            startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+            finish();
+        }
+    }
+
+
+
 }
